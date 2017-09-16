984382b12a916035953f3d540350fdbf91c39723
DeeptiSindhura <32009991+DeeptiSindhura@users.noreply.github.com>
2017-09-16 15:33:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+                                      Student st1=new Student(01,"DeeptiSindhura",new Date(1996,01,06),63.568);
+                                      Student st1=new Student(01,"Sindhu",new Date(1996,07,06),63.578);
+                                      Student st1=new Student(01,"SreeValli",new Date(1996,04,06),63.658);
+                                      Student st1=new Student(01,"Vennela",new Date(1996,01,06),63.458);
+	}
+
+}
